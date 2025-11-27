e85e5a78b210799beb5d40f4ca0714db5e2340df
Yury Imbro <109346141+yury-imbro-ox@users.noreply.github.com>
2025-11-27 19:31:13 +0100

    Update sqlinj.ts

--- sqlinj.ts
+++ sqlinj.ts
@@ -3,6 +3,7 @@ module.exports = function searchProducts () {
       let criteria: any = req.query.q === 'undefined' ? '' : req.query.q ?? ''
       criteria = (criteria.length <= 200) ? criteria : criteria.substring(0, 200)
       criteria.replace(/"|'|;|and|or/i, "")
+        // potentially problematic?
       models.sequelize.query(`SELECT * FROM Products WHERE ((name LIKE '%${criteria}%' OR description LIKE '%${criteria}%') AND deletedAt IS NULL) ORDER BY name`)
         .then(([products]: any) => {
           const dataString = JSON.stringify(products)
